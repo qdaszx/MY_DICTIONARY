9ab522fa8ef8ec07f3a7f2d108af0fdb5250cbf4
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-06-29 23:38:25 +0900

    fix view files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,127 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
+
 import Dictionary from "./Main";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: ["단어", "뜻", "예시"],
+      list: [
+        {
+          id: "list_0",
+          title: "정ㅋ벅ㅋ",
+          text: "정복하다",
+          example: "리액트 정ㅋ벅ㅋ",
+        },
+        {
+          id: "list_1",
+          title: "정ㅋ벅ㅋ",
+          text: "정복하다",
+          example: "리액트 정ㅋ벅ㅋ",
+        },
+      ],
     };
+
+    this.title = React.createRef();
+    this.text = React.createRef();
+    this.example = React.createRef();
+  }
+
+  addDictionary = () => {
+    let list = this.state.list;
+
+    const new_title = this.title.current.value;
+    const new_text = this.text.current.value;
+    const new_example = this.example.current.value;
+
+    this.setState({
+      list: [
+        ...list,
+        {
+          title: new_title,
+          text: new_text,
+          example: new_example,
+        },
+      ],
+    });
+  };
+
+  componentDidMount() {
+    console.log(this.title.current);
+    console.log(this.text);
+    console.log(this.example);
   }
 
   render() {
     return (
       <div className="App">
-        <h1>나의 단어장</h1>
-        <Dictionary />
+        <Container>
+          <Title>나만의 단어장</Title>
+          <Dictionary list={this.state.list} />
+        </Container>
+        <Input>
+          <Fix>단어 :</Fix>
+          <input type="text" ref={this.title} />
+          <Fix>설명 :</Fix>
+          <input type="text" ref={this.text} />
+          <Fix>예시 :</Fix>
+          <input type="text" ref={this.example} />
+          <button onClick={this.addDictionary}>추가하기</button>
+        </Input>
       </div>
     );
   }
 }
+
+const Fix = styled.p`
+  font-size: 15px;
+  font-weight: 600;
+  color: #a7bcecd2;
+  margin: 0;
+`;
+
+const Input = styled.div`
+  max-width: 500px;
+  min-height: 10vh;
+  background-color: #ffdfde;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 10px;
+  border: 1px solid #eee;
+  & * {
+    padding: 5px;
+  }
+  & input {
+    width: 70%;
+    margin-bottom: 3px;
+    &:focus {
+      border: 2px solid #6a7ba2;
+    }
+  }
+  & button {
+    margin: 0 0 0 20px;
+    width: 20%;
+    color: #ffdfde;
+    background-color: #6a7ba2;
+    border: 1px solid #6a7ba2;
+    border-radius: 5px;
+  }
+`;
+
+const Container = styled.div`
+  max-width: 500px;
+  min-height: 60vh;
+  background-color: #ffdfde;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 10px;
+  border: 1px solid #eee;
+`;
+
+const Title = styled.h1`
+  color: #6a7ba2;
+  text-align: center;
+`;
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,62 @@
 import React from "react";
+import styled from "styled-components";
 
 const Dictionary = (props) => {
+  const my_list = props.list;
+
   return (
-    <div>
-      <h1>단어</h1>
-    </div>
+    <ListStyle>
+      {my_list.map((list, index) => {
+        console.log(list);
+        return (
+          <div>
+            <ItemStyle key={index}>
+              <Fix>단어 : </Fix>
+              {list.title}
+            </ItemStyle>
+            <ItemStyle key={index}>
+              <Fix>설명 : </Fix>
+              {list.text}
+            </ItemStyle>
+            <ItemStyle key={index}>
+              <Fix>예시 : </Fix>
+              {list.example}
+            </ItemStyle>
+            <Line />
+          </div>
+        );
+      })}
+    </ListStyle>
   );
 };
+
+const Line = styled.hr`
+  border: 1px solid #ffdfde;
+  padding: 10px;
+  background-color: #ffdfde;
+  margin: 0;
+`;
+
+const Fix = styled.span`
+  font-size: 15px;
+  font-weight: 600;
+  color: #ffdfde;
+  margin: 0 4px 0 0;
+`;
+
+const ListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-color: #fff;
+`;
+
+const ItemStyle = styled.div`
+  padding: 16px;
+  margin: 2px;
+  background-color: #a7bcecd2;
+`;
 
 export default Dictionary;
