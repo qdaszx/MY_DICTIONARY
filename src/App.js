27978636036e64b9efe9fdd9e6c93e450dd9ec56
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-06-29 12:11:09 +0900

    add Main.js and connect App.js and Main.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dictionary from "./Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ["단어", "뜻", "예시"],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>나의 단어장</h1>
+        <Dictionary />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dictionary = (props) => {
+  return (
+    <div>
+      <h1>단어</h1>
+    </div>
+  );
+};
+
+export default Dictionary;
